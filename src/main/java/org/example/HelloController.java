f39ecfde2e076517c247449889ad37419c672d34
susovan120 <susovanmandal3@gmail.com>
2021-01-23 22:00:12 +0530

    Changes in HelloController.java

--- src/main/java/org/example/HelloController.java
+++ src/main/java/org/example/HelloController.java
@@ -3,12 +3,14 @@ package org.example;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.websocket.server.PathParam;
+
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping("/hello")
+    public String index(@PathParam("a") String a) {
+        return "Greetings from " + a;
     }
 
 }
